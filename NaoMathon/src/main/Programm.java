ce53016e6521e593d9367a2f3eabc3af7342e395
Jocelyn Tonnelier <jocelyn.tonnelier@gmail.com>
2015-06-18 13:23:41 +0200

    Integration helper, suppression mp3

--- NaoMathon/src/main/Programm.java
+++ NaoMathon/src/main/Programm.java
@@ -3,40 +3,31 @@ package main;
 import java.util.concurrent.TimeUnit;
 import com.aldebaran.qimessaging.*;
 import com.aldebaran.qimessaging.Object;
+import com.aldebaran.qimessaging.helpers.al.ALAudioPlayer;
+import com.aldebaran.qimessaging.helpers.al.ALTextToSpeech;
 
 /**
  * Created by Jocelyn on 02/06/2015.
  */
 public class Programm {
 
-  public static String NAO_IP = "hal.local";
+  public static String NAO_IP = "172.16.6.117";
   public static int NAO_PORT = 9559;
+  private static ALTextToSpeech tts;
+  private static ALAudioPlayer audioService;
 
   public static void main (String Args[]){
     try {
-      System.out.println("FONCTION ON");
       Session session = new Session();
-      Future<Void> future = session.connect("tcp://172.16.6.117:9559");
+      Future<Void> future = session.connect("tcp://"+NAO_IP +":"+NAO_PORT);
       future.get();
       //Declare Variable Object
-      com.aldebaran.qimessaging.Object tts = null;
-      com.aldebaran.qimessaging.Object bm = null;
-      com.aldebaran.qimessaging.Object listen = null;
-      tts = session.service("ALAudioPlayer");
+      tts = new ALTextToSpeech(session);
+      audioService = new ALAudioPlayer(session);
 
+      tts.say("Test");
+      audioService.playFile("/home/epsi3/Bureau/test/cri.wav");
       //Create Bundle Future
-
-
-      boolean ping = tts.<Boolean>call("ping").get();
-      if (!ping) {
-        System.out.println("Could not ping TTS");
-      } else {
-        System.out.println("Ping ok");
-      }
-
-      System.out.println("Calling say");
-      //tts.call("say", "Hello, world");
-      tts.call("playFile", "//home//cri.mp3");
     } catch (Exception e) {
       e.printStackTrace();
     }
