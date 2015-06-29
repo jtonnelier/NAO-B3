c275b8d32ca25d0d53ae1dc69a32e920803abf58
Jocelyn Tonnelier <jocelyn.tonnelier@gmail.com>
2015-06-29 15:44:17 +0200

    Update Jar et leds

--- NaoMathon/src/main/Programm.java
+++ NaoMathon/src/main/Programm.java
@@ -1,6 +1,7 @@
 package main;
 
 import com.aldebaran.qimessaging.*;
+import com.aldebaran.qimessaging.helpers.al.ALLeds;
 import com.aldebaran.qimessaging.helpers.al.ALRobotPosture;
 
 
@@ -15,11 +16,13 @@ public class Programm {
   public static String NAO_IP = "172.16.6.117";
   public static int NAO_PORT = 9559;
   public static ALRobotPosture robotPosture;
+  public static ALLeds leds;
 
   public static void main (String Args[]) throws InterruptedException, CallError {
 
     try {
       robotPosture.goToPosture("Stand", (float) 1.0);
+      leds.rasta((float) 5.0);
       Session session = new Session();
       Future<Void> future = null;
       future = session.connect("tcp://" + NAO_IP + ":" + NAO_PORT);
